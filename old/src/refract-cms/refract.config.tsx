b7c1458ca640887eee25f22a7662beeebe0bd4c4
Alex Bechmann <alex.bechmann@outlook.com>
2018-12-16 02:17:15 +0100

    delete old

--- old/src/refract-cms/refract.config.tsx
+++
@@ -1,8 +0,0 @@
-import { configure } from '@refract-cms/core';
-import { ProductSchema } from './products/product.model';
-import { NewsArticleSchema } from './news/news-article.model';
-import { SettingsSchema } from './settings/settings.model';
-
-export const config = configure({
-  schema: [ProductSchema, NewsArticleSchema, SettingsSchema]
-});
