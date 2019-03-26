e5221230f427c4a260de2202169a08ff7a4a2c27
Alex Bechmann <alex.bechmann@outlook.com>
2019-03-26 20:56:43 +0100

    removed some refs

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -24,12 +24,12 @@ const server = express()
         filesPath: process.env.FILES_DIR || 'files/',
         auth: {
           adminCredentials: {
-            username: 'ryomtand',
+            username: 'refract',
             password: 'Sc4JIlX0p5DM'
           },
           jwt: {
-            issuer: 'ryomtand',
-            secret: 'TjFggx2`dCjvH$/6'
+            issuer: 'refract-cms-example-dev',
+            secret: 'I4jni8zuRyWC-dev'
           }
         },
         publicGraphql: [
@@ -57,7 +57,7 @@ const server = express()
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta charSet='utf-8' />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
-    <title>Ryomtand</title>
+    <title>Refract-Cms Example</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
       ${
